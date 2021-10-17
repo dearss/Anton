23042e7ed07ec5e41b79736aa7d6087bf1475faf
dearss <baran2016@inbox.ru>
2021-10-17 17:10:41 +0300

    lab 2

---
+++ LB1/LB1/A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LB1
+{
+    class A
+    {
+        protected double[] x;
+
+        public double[] Function(double a, double b)
+        {
+            if (a == 0)
+            {
+                throw new Exception("Нет корней");
+            }
+
+            return x = new double[] { (-b / a) };
+        }
+    }
+}
