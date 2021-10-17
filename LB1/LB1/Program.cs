1e679e486693b043f47202d18b29608d46ed40f6
dearss <baran2016@inbox.ru>
2021-10-17 17:04:01 +0300

    lab 1

---
+++ LB1/LB1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LB1
+{
+    class Program
+    {
+        class A
+        {
+
+        }
+        class B : A
+        {
+            public B(object a)
+            {
+                this.a = a;
+            }
+            public object a;
+        }
+        class C : B
+        {
+            public C(object a, object b, object c) : base(a)
+            {
+                this.b = b;
+                this.c = c;
+            }
+            public object b;
+            public object c;
+        }
+        static void Main(string[] args)
+        {
+            C c = new C(new A(), new A(), new B(new A()));
+        }
+    }
+}
